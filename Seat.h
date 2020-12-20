cd67730596e59203b8c9263e4c42535a39b6a60b
hanahh080601 <70936440+hanahh080601@users.noreply.github.com>
2020-12-20 18:20:49 +0700

    Update Seat.h

--- Seat.h
+++ Seat.h
@@ -1,18 +1,25 @@
-﻿#pragma once
+#pragma once
+#include <iostream>
+using namespace std;
+#include <iomanip>
+#include <windows.h>
+#include <conio.h>
 #include <string>
 class Seat
 {
-	int seat_id ;
-	int	seat_type_id;
-	int	room_id;
-	char seat_row[5];
-	char seat_number[5];
-	int seat_status;
-public:
-	Seat(int = 0, int =0 , int =0, char*, char*, int = 0);
-	~Seat();
-	int getID();
-	int getStatus(); // ... cái chi cần dùng thì viết get ra
-	string setQueryInsert();
+	private:
+		int seat_id;
+		int seatType_id;
+		int room_id;
+		char seat_row[5];
+		char seat_number[5];
+		int seat_status;
+	public:
+		Seat();
+		Seat(char*, char*, int=0, int = 0, int = 0, int = 0);
+		~Seat();
+		int getSeatID();
+		int getStatus();
+		string insertQuery();
+		void Show();
 };
-

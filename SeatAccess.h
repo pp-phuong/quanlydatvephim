32d9a81bab19fa6c28b7b079517cb908f7c14929
hanahh080601 <70936440+hanahh080601@users.noreply.github.com>
2020-12-20 22:35:18 +0700

    Update SeatAccess.h

--- SeatAccess.h
+++ SeatAccess.h
@@ -2,14 +2,14 @@
 #include "DatabaseAccess.h"
 #include "Seat.h"
 class SeatAccess :
-    public DatabaseAccess
+	public DatabaseAccess
 {
-	public:
-	void Insert(); // thêm seat 
+public:
 	void Select(Seat*&); // đổ dữ liệu full seat về
 	void Show(); // show seat row vs number vs loại seat
-	void Update(); // chỉnh sửa seatType của seat
-	void Delete(); // xóa seat
-	int CountRow(); // đếm hàng ghế
+	int Count(int, int); // đếm hàng ghế theo phòng
+	bool Insert(); // thêm seat 
+	bool Update(); // chỉnh sửa seatType của seat
+	bool Delete(); // xóa seat
 };
 

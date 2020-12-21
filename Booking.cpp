d6cd622e31531aa776852cec8c2ea31bae4f5840
hanahh080601 <70936440+hanahh080601@users.noreply.github.com>
2020-12-21 17:46:41 +0700

    Create Booking.cpp

---
+++ Booking.cpp
@@ -0,0 +1,56 @@
+#include "Booking.h"
+
+Booking::Booking()
+{
+	this->booking_id = 0;
+	this->account_id = 0;
+	this->schedule_id = 0;
+	this->seat_id = 0;
+}
+
+Booking::Booking(int bookingID, int accountID, int scheduleID, int seatID)
+{
+	this->booking_id = bookingID;
+	this->account_id = accountID;
+	this->schedule_id = scheduleID;
+	this->seat_id = seatID;
+}
+
+Booking::~Booking()
+{}
+
+int Booking::getBookingID()
+{
+	return this->booking_id;
+}
+
+int Booking::getAccountID()
+{
+	return this->account_id;
+}
+
+int Booking::getScheduleID()
+{
+	return this->schedule_id;
+}
+
+int Booking::getSeatID()
+{
+	return this->seat_id;
+}
+
+string Booking::insertQuery()
+{
+	string t_query;
+	string t_booking_id = to_string(this->booking_id);
+	string t_account_id = to_string(this->account_id);
+	string t_schedule_id = to_string(this->schedule_id);
+	string t_seat_id = to_string(this->seat_id);
+	t_query = t_booking_id + "','" + t_account_id + "','" + t_schedule_id + "','" + t_seat_id + "')";
+	return t;
+}
+
+void Booking::Show()
+{}
+
+

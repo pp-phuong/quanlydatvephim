c387f5e50ac2e17c6ed4d8ae85463ea6f2ccd3bf
hanahh080601 <70936440+hanahh080601@users.noreply.github.com>
2020-12-21 18:30:56 +0700

    Create BookingAccess.h

---
+++ BookingAccess.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Booking.h"
+#include "DatabaseAccess.h"
+class BookingAccess : public DatabaseAccess
+{
+	public:
+        void Select(Booking*&);
+        int Count();
+        bool Insert();
+        int Search(int);
+        bool Update();
+        bool Delete();
+        void Show();
+};
+

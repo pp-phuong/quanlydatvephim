473c11ca499258c3f29be41986979196ec5b07c3
hanahh080601 <70936440+hanahh080601@users.noreply.github.com>
2020-12-22 01:32:53 +0700

    Update Room.h

--- Room.h
+++ Room.h
@@ -7,17 +7,18 @@ using namespace std;
 #include <string>
 class Room
 {
-private:
-	int room_id;
-	char room_name[5];
-public:
-	Room();
-	Room(char*, int = 0);
-	~Room();
-	int getRoomID();
-	char* getRoomName();
-	string insertQuery();
-	void Show();
+	private:
+		int room_id;
+		char room_name[5];
+	public:
+		Room();
+		Room(char*, int=0);
+		~Room();
+		int getRoomID();
+		char* getRoomName();
+		void setRoom();
+		string insertQuery();
+		void Show();
 };
 
 

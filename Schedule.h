8eb1dbe4a933827f437146c1d38dc1f30ed56bc6
Phượng Trần Thị <tranphiphuong2763@gmail.com>
2020-12-21 22:56:59 +0700

    add schedule

--- Schedule.h
+++ Schedule.h
@@ -7,22 +7,22 @@ using namespace std;
 #include <string>
 class Schedule
 {
-	private:
-		int schedule_id;
-		int movie_id;
-		int room_id;
-		char schedule_date[15];
-		char schedule_start[15];
-		char schedule_end[15];
-	public:
-		Schedule();
-		Schedule(char*, char*, char*, int = 0, int = 0, int = 0);
-		~Schedule();
-		int getScheduleID();
-		char* getSDate();
-		char* getStart();
-		char* getEnd();
-		string insertQuery();
-		void Show();
+private:
+	int schedule_id;
+	int movie_id;
+	int room_id;
+	char schedule_date[15];
+	char schedule_start[15];
+	char schedule_end[15];
+public:
+	Schedule();
+	Schedule(char*, char*, char*, int = 0, int = 0, int = 0);
+	~Schedule();
+	int getScheduleID();
+	char* getSDate();
+	char* getStart();
+	char* getEnd();
+	string insertQuery();
+	void Show();
 };
 

9f354c70988d920cccea9bdbfa0ca413b96605a1
hanahh080601 <70936440+hanahh080601@users.noreply.github.com>
2020-12-22 18:39:14 +0700

    Create ScheduleAccess.h

---
+++ ScheduleAccess.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Schedule.h"
+#include "Date.h"
+#include "DatabaseAccess.h"
+class ScheduleAccess : public DatabaseAccess
+{
+	public:
+        void Select(Schedule*&, int, int);
+        int Count(int);
+        bool Insert();
+        int Search(int, int); // search theo id_number
+        bool Update(int,int);
+        bool Delete();
+        void Show();
+        Schedule getSchedule(int, int);
+        int LastID();
+};
+

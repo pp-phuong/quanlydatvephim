ca88484c57fd2349877a885770edf3d850754685
hanahh080601 <70936440+hanahh080601@users.noreply.github.com>
2020-12-22 18:37:50 +0700

    Update Schedule.h

--- Schedule.h
+++ Schedule.h
@@ -5,26 +5,26 @@ using namespace std;
 #include <windows.h>
 #include <conio.h>
 #include <string>
-#include "MovieAccess.h"
-#include "RoomAccess.h"
 class Schedule
 {
-private:
-	int schedule_id;
-	int movie_id;
-	int room_id;
-	char schedule_date[15];
-	char schedule_start[15];
-	char schedule_end[15];
-public:
-	Schedule();
-	Schedule(char*, char*, char*, int = 0, int = 0, int = 0);
-	~Schedule();
-	int getScheduleID();
-	char* getSDate();
-	char* getStart();
-	char* getEnd();
-	string insertQuery();
-	void Show();
+	private:
+		int schedule_id;
+		int movie_id;
+		int room_id;
+		char schedule_date[15];
+		char schedule_start[15];
+		char schedule_end[15];
+	public:
+		Schedule();
+		Schedule(char*, char*, char*, int = 0, int = 0, int = 0);
+		~Schedule();
+		int getScheduleID();
+		char* getSDate();
+		char* getStart();
+		char* getEnd();
+		string insertQuery();
+		void setSchedule();
+		void Show();
 };
+
 
